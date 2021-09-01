6cecb605e4e4071c7bd2f028c7a34940c8916a32
Jourdelune <64205064+Jourdelune@users.noreply.github.com>
2021-09-01 19:31:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.maximize();
+  mainWindow.setMenu(null)
+  mainWindow.show();
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
